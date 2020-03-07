6064b8c976741f084609ad9d1bef54ce310fc9ec
huangjiaxi <huangjiaxi@oak-inc.com>
2020-03-08 03:26:33 +0800

    mock fs模块  测试读写成功

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,9 +1,50 @@
 // 声明是 fs的 jest 的假模块
 const fs = jest.genMockFromModule('fs');
+const _fs = jest.requireActual('fs');
 
-fs.x = ()=>{
-    console.log('hi')
-    return 'xxx';
+Object.assign(fs,_fs);
+
+let readMocks = {};
+
+fs.setReadFileMock = (path, error, data)=>{
+    readMocks[path] = [error,data];
+}
+
+// 覆盖 fs的 readFile()
+fs.readFile = (path, options, callBack) => {
+    // 如果你 fs.readFile('xxx',fn) 代表第二个参数是 callBack
+    if(callBack === undefined){
+        callBack = options
+    }
+
+    if(path in readMocks){
+        callBack(...readMocks[path])
+    }else{
+        _fs.readFile(path, options, callBack)
+    }
+}
+
+let writeMocks = {}
+
+fs.setWriteFileMock = (path, fn)=>{
+    writeMocks[path] = fn;
+}
+
+fs.writeFile = (path,data,options, callBack) => {
+    if(callBack === undefined){
+        callBack = options;
+    }
+    if(path in writeMocks){
+        writeMocks[path](path, data, options, callBack)
+    }else{
+        _fs.writeFile(path, data, options, callBack)
+    }
+
+}
+
+fs.clearMocks = ()=>{
+    readMocks = {};
+    writeMocks = {};
 }
 
 module.exports = fs;
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -3,7 +3,25 @@ const fs = require('fs');
 jest.mock('fs'); 
 
 describe("db",()=>{
-    it("can read",()=>{
-        expect(fs.x()).toBe('xxx')
+    afterEach(()=>{
+        // 每个it 执行之后
+        fs.clearMocks();
+    })
+    it("can read", async () =>{
+        const data = [{title:"hi",done:true}]
+        fs.setReadFileMock('/xxx',null,JSON.stringify(data));
+        const list = await db.read('/xxx');
+        expect(list).toStrictEqual(data)
+    })  
+
+    it("can wriete", async () =>{
+        let fakeFile;
+        fs.setWriteFileMock('/yyy',(path, data, options, callBack)=>{
+            fakeFile = data;
+            callBack(null)
+        })
+        const list = [{title:"见欧阳娜娜",done:true}, {title:"见迪丽热巴",done:true}]
+        await db.write(list,'/yyy');
+        expect(fakeFile).toBe(JSON.stringify(list) + "\n");
     })  
 })
